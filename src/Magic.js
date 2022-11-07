aa6f1de37e45715ae8fd5fd2667689066b63b518
Josh McCormack <joshgmc@gmail.com>
2022-11-07 01:41:54 -0500

    videos, buttons added

--- src/Magic.js
+++ src/Magic.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import axios from "axios";
 
 
@@ -9,34 +9,51 @@ export default function Magic(){
             .then((res)=> {
                 console.log(res)
                 setData(res)
-                console.log(data)
+                
             })
             .catch((err)=>console.error(err));
     
     },[])
+    const [range, setRange] = useState([1,11])
+    console.log(range)
+
+
     return(
         <>  
+            <div className="buttons">
+                {range[0]<10?
+                <Fragment />:
+                <button className='previous' onClick={() => setRange([range[0]-10, range[1]-10])}>Previous 10</button>}
+                <button onClick={() => setRange([range[0]+10, range[1]+10])}>Next 10</button>
+            </div>
             {data.data ?
                 data.data.values.map((item,i)=>{
-                    if (i>0){
+                    if (i >= range[0] && i < range[1]){
                         return (
-                            <div key={i} className="upload">
+                            <div key={item[3]} className="upload">
+                                <iframe width='560' height='315' src={"https://www.youtube.com/embed/" + item[3].slice(item[3].indexOf('=')+1)} frameBorder="0"></iframe>
                                 <ul>
-                                    <li>{item[0]}</li>
+                                    <li><a href={item[3]} target='_blank' rel="noreferrer">{item[0]}</a></li>
                                     <li>{item[1]}</li>
                                     <li>{item[2]}</li>
                                 </ul>
                             </div>
                         )
                     }
-                }) : 'loading....'
+                }) : (
+                    <h3>{'loading....'}</h3>
+                )
             }
-            
+            <div className="buttons">
+                {range[0]<10?
+                <Fragment />:
+                <button className='previous' onClick={() => setRange([range[0]-10, range[1]-10])}>Previous 10</button>}
+                <button onClick={() => setRange([range[0]+10, range[1]+10])}>Next 10</button>
+            </div>
         </>
     )
 
 }
-
 
 
 
