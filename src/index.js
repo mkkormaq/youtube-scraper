df0afd2cb9d10cc7d9de6ee6717322e95dfe36cb
Josh McCormack <joshgmc@gmail.com>
2022-11-07 12:26:05 -0500

    added portfolios, python icon, favicon

--- src/index.js
+++ src/index.js
@@ -9,10 +9,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <header>
+      <div id="mySidepanel" className="sidepanel">
+        <a href="#" className="closebtn" onClick={()=>closeNav()}>&times;</a>
+        <a href="https://sethmerck.netlify.app/">Seth Merck Porfolio</a>
+        <a href="https://mkkormaq.netlify.app/">Josh McCormack Portfolio</a>
+      </div>
+      <div className='hamburger'>
+        <a id="menu" href='#' className='openbtn' onClick={()=>openNav()}>&#9776;</a>
+      </div>
       <div className='header-logo'>
         <img src="logo192.png" alt="react logo" />
+        <h2><a href="#banner">ScrapeTube</a></h2>
+        <img src="logo_py.png" alt="react logo" />
       </div>
-      <h2><a href="#banner">ScrapeTube</a></h2>
       <div className='authors'>
         <span>CODERS:</span>
         <ul>
@@ -35,6 +44,13 @@ root.render(
   </React.StrictMode>
 );
 
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
